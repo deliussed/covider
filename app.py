aa7960ff5dd64cd4b599ab84b59a7366a205cf08
deliussed <tleyman@gmail.com>
2021-07-31 18:40:23 +0100

    flask container

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 import covider
+import json
 from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+
+app = Flask(__name__)
+api = Api(app)
 
 print ("ready")
 
 covider = covider.Covider()
 
-print (covider.get_results())
+#print (covider.get_results())
+
+class RNumbers(Resource):
+    # methods go here
+    def get(self):
+        #covider = covider.Covider()
+        data    = covider.get_results()
+        #data    = json.loads(data) # convert dataframe to dict
+        return {'data': data}, 200  # return data and 200 OK
+
+api.add_resource(RNumbers, '/rs')  # rs is entrypoint
+
+if __name__=="__main__":
+    app.run(host='127.0.0.9',port=4455,debug=True)
+
+
 
 #app = Flask(__name__)
 
